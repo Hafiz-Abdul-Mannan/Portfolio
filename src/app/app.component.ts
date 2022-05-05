bcdf2f3dd90750cdb0473c5a4478a2711751e5e0
Hafiz-Abdul-Mannan <hafiz.abdul.mannan@outlook.com>
2022-05-05 22:24:36 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Portfolio';
+  images: string[] = ['../assets/images/WeatherApp.jpg', '../assets/images/RecipeBook.jpg', '../assets/images/FoodMine.jpg'];
 }
